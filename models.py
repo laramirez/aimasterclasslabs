8fefd9aa07befb51c90d75ceabdee683c922b37b
Lara RAMIREZ <lramirez@e1r2p7.42.fr>
2017-11-27 18:00:15 +0100

    yoooo

--- models.py
+++ models.py
@@ -15,18 +15,18 @@ class Net(nn.Module):
             nn.BatchNorm2d(28),
             nn.ReLU(),
             nn.MaxPool2d(2))
-        self.layer3 = nn.Sequential(
-            nn.Conv2d(28, 28, kernel_size=3, padding=1),
-            nn.BatchNorm2d(28),
-            nn.ReLU(),
-            nn.MaxPool2d(2))
+        # self.layer3 = nn.Sequential(
+        #     nn.Conv2d(28, 28, kernel_size=3, padding=1),
+        #     nn.BatchNorm2d(28),
+        #     nn.ReLU(),
+        #     nn.MaxPool2d(2))
         self.fc = nn.Linear(7*7*28, 64)
-        self.fc2 = nn.Linear(64, 64)
+        # self.fc2 = nn.Linear(64, 64)
 
     def forward(self, x):
         out = self.layer1(x)
         out = self.layer2(out)
         out = out.view(out.size(0), -1)
         out = self.fc(out)
-        out = self.fc2(out)
+        # out = self.fc2(out)
         return F.log_softmax(out)
